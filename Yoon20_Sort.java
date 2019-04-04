871ca49edf32099b181a97f55e2660796b243a87
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-04-04 23:38:21 +0900

    Add files via upload

--- Yoon20_Sort.java
+++ Yoon20_Sort.java
@@ -5,11 +5,13 @@ import java.util.Arrays;
 public class Yoon20_Sort {
 
 	public static void main(String[] args) {
-		Person[] ar= new Person[4];
+		Person[] ar= new Person[6];
 		ar[0] = new Person("goo", 23);
 		ar[1] = new Person("chan", 42);
-		ar[2]= new Person("moon", 35);
+		ar[2] = new Person("moon", 35);
 		ar[3] = new Person("lee", 23);
+		ar[4] = new Person("Kim", 18);
+		ar[5] = new Person("Ann", 42);
 		Arrays.sort(ar); // 정렬함수.
 		for(Person p : ar) {
 			System.out.println(p);
@@ -19,8 +21,8 @@ public class Yoon20_Sort {
 			System.out.println("true");
 		if(a.equals(b))
 			System.out.println("equals  true"); // 객체 ㄴㄴ
-		
-		System.out.println(ar[Arrays.binarySearch(ar, new Person("who are you?",23))]);
+		int idx =Arrays.binarySearch(ar, new Person("moon", 42));
+		System.out.println(idx+"\t" + ar[Arrays.binarySearch(ar, new Person("chan",42))]);
 	}
 
 }
@@ -38,6 +40,9 @@ class Person implements Comparable{
 	@Override
 	public int compareTo(Object o) {
 		Person p = (Person)o;
-		return this.age- p.age;
+		if(this.name.equals(p.name))
+			return this.age- p.age;
+		else
+			return this.name.compareTo(p.name); // 각각의 문자열의 대한 판단도 해줘야함.
 	}
 }
