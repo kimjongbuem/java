6143e5a3f1beb429ce9e0a30bf5fffe6958e4fb3
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-04-05 19:38:54 +0900

    Add files via upload

---
+++ Yoon23_CollectionFrameWork_List.java
@@ -0,0 +1,40 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Yoon23_CollectionFrameWork_List {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> list = new LinkedList<>(); //ArrayList
+		List<String> test = new ArrayList<>();
+		List<String> test2 = Arrays.asList("Toy","Car","Robot");
+		test2 = new ArrayList<>(list);
+		// 한번에 초기화... immutable 인스턴스임 고정이라... ㄷㄷ->
+		// 다시만듬.
+		list.add("Toy");
+		list.add("Hello");
+		list.add("Box");
+		list.add("Robot");
+		for(String s : list)
+			System.out.println(s+"\t");
+		String str;
+		Iterator<String> itr = list.iterator(); // 반복자
+		while(itr.hasNext()) 
+		{	
+			str= itr.next();
+			if(str.equals("Box"))
+				itr.remove();
+		//System.out.println(itr);
+			// 컬렉션 프레임웤은 반복자를 통해 이렇게 참조가 가능하구나~ 더미노드가 존재한다  // 
+		}
+		System.out.println();
+		for(String s : list) // for each , iterator 기반.
+			System.out.println(s+"\t");
+	}
+
+}
