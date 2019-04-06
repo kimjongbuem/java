ac49279b86e32c59515bb0f7fed2808579fabd59
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-04-06 12:43:34 +0900

    Add files via upload

--- Yoon23_CollectionFrameWork_List.java
+++ Yoon23_CollectionFrameWork_List.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Yoon23_CollectionFrameWork_List {
 
@@ -35,6 +36,29 @@ public class Yoon23_CollectionFrameWork_List {
 		System.out.println();
 		for(String s : list) // for each , iterator 기반.
 			System.out.println(s+"\t");
+		
+		System.out.println();
+		System.out.println();
+		// 양방향 반복자 //
+		List<String> dList = Arrays.asList("Toy","Box","Robot","Box");
+		dList = new ArrayList<>(dList);
+		
+		ListIterator<String> dIter = dList.listIterator();
+		while(dIter.hasNext()) {
+			str = dIter.next();
+			System.out.print(str + "\t");
+			if(str.equals("Toy"))
+				dIter.add("Toy2");
+		}
+		System.out.println();
+		
+		while(dIter.hasPrevious()) {
+			str = dIter.previous();
+			System.out.print(str + "\t");
+			if(str.equals("Robot"))
+				dIter.add("Robot2");
+		}
 	}
 
 }
+
