e6e5b8828919814e3faf77fdd05a53a1af453b3e
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-04-07 22:44:02 +0900

    Add files via upload

---
+++ Yoon23_4_Queue.java
@@ -0,0 +1,31 @@
+package CollectionFrameWork;
+import java.util.*;
+public class Yoon23_4_Queue {
+
+	public static void main(String[] args) {
+		/*Queue method
+		 * 예외처리알고리즘적용
+		 * boolean add(E e)
+		 * E remove()
+		 * E element
+		 * 
+		 * 기능은 같은데 예외를 발생안시킴 그대신 반환 값을 말해주어서 판단하게함.
+		 * boolean offer(E e) // 넣을공간없으면 false
+		 * E poll // 꺼낼 대상 없으면 null
+		 * E peek // 확인하기, 확인할 대상 없으면 null임.
+		 */
+		Queue<String> que = new LinkedList<>(); // queue interface 리스트가 내부인 큐.
+		
+		que.offer("BOX");
+		que.offer("TOY");
+		que.offer("Robot");
+		System.out.println("next :" +que.peek());
+		
+		System.out.println("next :" +que.poll());
+		System.out.println("next :" +que.poll());
+		System.out.println("next :" +que.peek());
+		System.out.println("next :" +que.poll());
+		System.out.println("next :" +que.poll());
+	}
+
+}
