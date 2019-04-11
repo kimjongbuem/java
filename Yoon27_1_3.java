07fc89c088db5009e644e7a6243a439321352982
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-04-11 19:21:57 +0900

    Add files via upload

--- Yoon27_1_3.java
+++ Yoon27_1_3.java
@@ -1,7 +1,11 @@
 package CH27;
 import java.util.*;
+import java.util.function.Consumer;
 import java.util.function.IntPredicate;
+import java.util.function.IntSupplier;
+import java.util.function.ObjIntConsumer;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 public class Yoon27_1_3 {
 
 	public static void main(String[] args) {
@@ -11,7 +15,20 @@ public class Yoon27_1_3 {
 		sum = sum_List(n -> n%2==0, list);
 		sum = sum_List(n-> n%2 != 0, list);
 		System.out.println(sum);
-
+//		Supplier<Integer>supplier = () -> {
+//			Random rand = new Random();
+//			return rand.nextInt(50);
+//		};
+		list = makeIntList(() -> {
+			Random rand = new Random();
+			return rand.nextInt(50);
+		}, 10);
+		System.out.println(list);
+		
+		Consumer<String> c = s-> System.out.println(s);
+		c.accept("Hi Hello");
+		ObjIntConsumer<String> objInts = (T, value) -> System.out.println(value+"\t"+T);
+		objInts.accept("<php>", 123);
 	}
 	public static int sum_List(IntPredicate p, List<Integer> list) {
 		int sum = 0;
@@ -19,8 +36,23 @@ public class Yoon27_1_3 {
 			if(p.test(n))
 				sum+=n;
 		}
-		return sum;  // 오토박싱 언박싱 과정 없다. 빠르다
+		return sum;  // 오토박싱 언박싱 과정 없다. 빠르다 권고사항임
 	}
+	public static List<Integer> makeIntList(IntSupplier s, int n){
+		List<Integer> list = new LinkedList<>();
+		for(int i = 0;i<n;i++) {
+			list.add(s.getAsInt());
+		}
+		return list;
+	}
+//	public static List<Integer> makeIntList(Supplier<Integer> s, int n){
+//		List<Integer> list = new LinkedList<>();
+//		for(int i = 0;i<n;i++) {
+//			list.add(s.get());
+//		}
+//		return list;
+//	}
+	//public static int sum_List(BiPredicate p, List<Integer> list)
 //	public static int sum_List(Predicate<Integer>p, List<Integer> lst) {
 //		int sum = 0;
 //		for(int n : lst) {
