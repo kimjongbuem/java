40056dce1fe862c4fec55ad7fd91096eb9fb0cf5
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-04-14 17:06:05 +0900

    Add files via upload

--- Yoon32_3.java
+++ Yoon32_3.java
@@ -4,13 +4,54 @@ public class Yoon32_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try(Writer out = new FileWriter("data.txt")){
-			out.write(370);
-			out.write('한');
+		try(Writer out = new FileWriter("data2.txt")){
+			for(int ch = (int)'A'; ch < (int)('Z' + 1); ch++) {
+				out.write(ch);
+			}
 			}
 		catch(Exception e){
 			e.printStackTrace();
 		}
+		
+		try(Reader out = new FileReader("data.txt")){
+			int ch;
+			while(true) {
+				ch = out.read();
+				if(ch == -1)
+					break;
+				System.out.print((char)ch);
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		System.out.println();
+		
+		String ks = "공부는 힘들다";
+		String es = "Studying is hard";
+		try(BufferedWriter out = new BufferedWriter(new FileWriter("data3.txt"))){
+			out.write(ks);
+			out.newLine();
+			out.write(es);
+			}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		try(BufferedReader out = new BufferedReader(new FileReader("data3.txt"))){
+			String str;
+			while(true) {
+				str = out.readLine(); // 개행기준으로 앞뒤로 문자열을 읽어들인다.
+				if(str == null)
+					break;
+				System.out.println(str);
+			}
+			}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
---
+++ Yoon32_4.java
@@ -0,0 +1,38 @@
+package CH32_IO_Stream;
+import java.io.*;
+public class Yoon32_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Boxing b1 = new Boxing("Robot");
+		Boxing b2 = new Boxing("Monster");
+		try(ObjectOutputStream oo = new ObjectOutputStream(new FileOutputStream("data3.txt"))) {
+			oo.writeObject(b1);
+			oo.writeObject(b2);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		try(ObjectInputStream oo = new ObjectInputStream(new FileInputStream("data3.txt"))) {
+			Boxing temp;
+			while(true) {
+				temp = (Boxing)oo.readObject();
+				if(temp == null)
+					break;
+				System.out.println(temp.toString());
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+class Boxing implements java.io.Serializable{
+	String s;
+	public Boxing(String s) {this.s = s;}
+	public String getBox() {return s;}
+	
+	public String toString() {
+		return s;
+	}
+}
