66a9736890c155f6b4913880e78afcfa4e2b9e8d
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-04-19 23:34:28 +0900

    Add files via upload

--- idontstudyJAVA/AboutStaticWrapping.java
+++ idontstudyJAVA/AboutStaticWrapping.java
@@ -1,5 +1,8 @@
 package idontstudyJAVA;
 import java.math.*;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 public class AboutStaticWrapping {
 
 	public static void main(String[] args) {
@@ -16,7 +19,27 @@ public class AboutStaticWrapping {
 		BigInteger mulResult = bigVal2.multiply(bigVal1);
 		System.out.println(addResult);
 		System.out.println(mulResult);
+		double e3 = 1.6;
+		double e4 = 0.1;
+		BigDecimal e1 = new BigDecimal("1.6");
+		BigDecimal e2 = new BigDecimal("0.1");
+		System.out.println(e1.multiply(e2)); // 오차 ㄴㄴ
+		System.out.println(e3*e4); // 오차 @
+		Random rand = new Random();
+		Scanner sc =new Scanner(System.in);
+		System.out.println("정수 2개 입력 :");
+		int data1 = sc.nextInt(); int data2 = sc.nextInt(); // data1 > data2
+		int val; double d;
+		for(int i = 0;i<10;i++) {
+			val = rand.nextInt(data1-data2) + data2;
+			 System.out.print(val+"\t");
+			 d= Math.random();
+			 System.out.print(d+"\t");
+		}
+		String strData = "11:22:33:44:55";
 		
-		System.out.println("");
+		StringTokenizer st = new StringTokenizer(strData,":");
+		while(st.hasMoreTokens())
+			System.out.println(st.nextToken());
 	}
 }
