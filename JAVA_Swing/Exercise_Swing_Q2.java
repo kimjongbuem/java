39431e00781a0c549352e3372e0457877de42edc
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-04-22 14:49:11 +0900

    Add files via upload

--- JAVA_Swing/Exercise_Swing_Q2.java
+++ JAVA_Swing/Exercise_Swing_Q2.java
@@ -10,6 +10,14 @@ public class Exercise_Swing_Q2 {
 
 	public static void main(String[] args) {
 		MyJFrame frm = new MyJFrame("Second Swing");
+		frm.setVisible(true);
+	}
+}
+class MyJFrame extends JFrame implements MouseListener{
+	MyJFrame(String s){
+		super(s);
+		super.setBounds(120,120,400,400);
+		super.setLayout(new FlowLayout());
 		JButton btn1 = new JButton("myBt1");
 		RevisedMouseEventHandler listener = new RevisedMouseEventHandler();
 		btn1.addMouseListener(listener);
@@ -17,16 +25,37 @@ public class Exercise_Swing_Q2 {
 		JButton btn3 = new JButton("myBtn3");
 		btn2.addMouseListener(listener);
 		btn3.addMouseListener(listener);
-		frm.add(btn1); frm.add(btn2);                       
-		frm.add(btn3);
-		frm.setVisible(true);
+		this.add(btn1); this.add(btn2);                       
+		this.add(btn3);
+		addMouseListener(this);
 	}
-}
-class MyJFrame extends JFrame{
-	MyJFrame(String s){
-		super(s);
-		super.setBounds(120,120,400,400);
-		super.setLayout(new FlowLayout());
-		addMouseListener(new FrameMouseEventHandler());
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		System.out.println("JFrame상에서 마우스 버튼 눌렸다 풀림~");
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
