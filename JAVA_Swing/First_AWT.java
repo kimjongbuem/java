1fec4ed1bec2b8eae78b24f6d2e5320c91825046
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-04-22 13:45:37 +0900

    Add files via upload

--- JAVA_Swing/First_AWT.java
+++ JAVA_Swing/First_AWT.java
@@ -1,17 +1,18 @@
 package JAVA_Swing;
+
 import java.awt.*;
-import javax.swing.*;
-public class FirstSwing {
+
+public class First_AWT {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JFrame frm = new JFrame("First Swing");
+		Frame frm = new Frame("First AWT");
 		frm.setBounds(120,120,400,100);
 		frm.setLayout(new FlowLayout());
 		
-		JButton btn1 = new JButton("myBt1");
-		JButton btn2 = new JButton("myBtn2");
-		JButton btn3 = new JButton("myBtn3");
+		Button btn1 = new Button("myBt1");
+		Button btn2 = new Button("myBtn2");
+		Button btn3 = new Button("myBtn3");
 		frm.add(btn1); frm.add(btn2);                       
 		frm.add(btn3);
 		frm.setVisible(true);
