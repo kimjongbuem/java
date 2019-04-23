289e8481fe73fdc4f7787e3c9f718aea79067368
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-04-23 13:24:49 +0900

    Add files via upload

---
+++ JAVA_Swing/BorderLayoutManager.java
@@ -0,0 +1,23 @@
+package JAVA_Swing;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class BorderLayoutManager {
+
+	public static void main(String[] args) {
+		JFrame frm = new JFrame("BorderLayout Test");
+		frm.setBounds(120,120,300,200);
+		frm.setLayout(new BorderLayout());
+		
+		frm.add(new JButton("EAST"), BorderLayout.EAST);
+		frm.add(new JButton("WEST"), BorderLayout.WEST);
+		frm.add(new JButton("CENTER"), BorderLayout.CENTER);
+		frm.add(new JButton("SOUTH"), BorderLayout.SOUTH);
+		frm.add(new JButton("NORTH"), BorderLayout.NORTH);
+		frm.setVisible(true);
+
+	}
+
+}
