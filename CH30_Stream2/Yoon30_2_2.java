6f673702c93320d4504c15e69e0c594d2b6278a6
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-04-28 23:56:38 +0900

    Add files via upload

--- CH30_Stream2/Yoon30_2_2.java
+++ CH30_Stream2/Yoon30_2_2.java
@@ -9,13 +9,16 @@ public class Yoon30_2_2 {
 		// 정렬 //
 		Stream.of("Box","Apple","Robot Taegon V")
 		.sorted().forEach(s->System.out.println(s));
-		
+		Stream.of(1,2,3,4,5).sorted().forEach(s->System.out.print(s));
+		System.out.println();
 		Stream.of("Box","Apple","Robot Taegon V").sorted((s1, s2) -> s2.length() - s1.length())
 		.forEach(s ->System.out.println(s));
-		
+//		
 		// 루핑 peek은 중간연산 forEach는  최종연산// 
 		IntStream.of(1,2,3,4,5).peek(d -> System.out.print(d+"\t")); //중간연산이라 최종연산없으면 실행 보류함
 		System.out.println(IntStream.of(1,2,3,4,5).peek(d -> System.out.print(d+"\t")).sum());
+		
+		IntStream.of(1,2,3,4,5,6).peek(a ->System.out.println(a)).sum();
 	}
-
+	
 }
